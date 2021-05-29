31b5984ef7c9a3bfd5a98ae05541d395a209636a
Ryely <ryely.moore@outlook.com>
2021-05-29 17:32:05 -0600

    First row mock up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var calendarEl = $("#calendar")
+
 var init = function() {
     // Create a variable for the current date
     var todayDate = moment().format("dddd, MMMM Do");
@@ -5,6 +7,12 @@ var init = function() {
     // Set the text for the currentDay div to todays date
     $("#currentDay").text(todayDate);
 
+    var rowEl = $("<div>").addClass("row");
+    var colTimeEl = $("<div>").addClass("col-2").text("9 AM");
+    var colTaskEl = $("<div>").addClass("col-8").text("Tasks Go Here");
+    var colSaveEl = $("<div>").addClass("col-2").text("Button Goes Here");
+    rowEl.append(colTimeEl, colTaskEl, colSaveEl);
+    calendarEl.append(rowEl);
 }
 
 init();
