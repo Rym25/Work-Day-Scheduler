9bf974d5cb3856350107f79c2d0fce9d8d65c1bc
Ryely <ryely.moore@outlook.com>
2021-05-29 17:49:54 -0600

    has rows for 9 to 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,30 @@ var init = function() {
     console.log(todayDate);
     // Set the text for the currentDay div to todays date
     $("#currentDay").text(todayDate);
-
-    var rowEl = $("<div>").addClass("row");
-    var colTimeEl = $("<div>").addClass("col-2").text("9 AM");
-    var colTaskEl = $("<div>").addClass("col-8").text("Tasks Go Here");
-    var colSaveEl = $("<div>").addClass("col-2").text("Button Goes Here");
-    rowEl.append(colTimeEl, colTaskEl, colSaveEl);
-    calendarEl.append(rowEl);
+    // Creates Calendar Time Blocks
+    for(var i = 0; i < 9; i++){
+        // creates a row element
+        var rowEl = $("<div>").addClass("row");
+        // creates the time column
+        if(i <= 2) {
+            var x = 9 + i;
+            var colTimeEl = $("<div>").addClass("col-2").text(x + "AM");
+        } else if (i === 3) {
+            var x = 12
+            var colTimeEl = $("<div>").addClass("col-2").text(x + "PM");
+        } else {
+            var x = i - 3;
+            var colTimeEl = $("<div>").addClass("col-2").text(x + "PM");
+        }
+        // creates the task div
+        var colTaskEl = $("<div>").addClass("col-8").text("Tasks Go Here").attr("id", x + "task");
+        // creates the button div
+        var colSaveEl = $("<div>").addClass("col-2").text("Button Goes Here");
+        // appends the created columns to the row
+        rowEl.append(colTimeEl, colTaskEl, colSaveEl);
+        // appends the created row to the calendarEl in the html
+        calendarEl.append(rowEl);
+    }
 }
 
 init();
