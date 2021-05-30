86b942222e33514a73eb4e7ab244d98a18c86b74
Ryely <ryely.moore@outlook.com>
2021-05-29 18:25:39 -0600

    added some styling and event listner to the task section

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,23 +13,28 @@ var init = function() {
         // creates the time column
         if(i <= 2) {
             var x = 9 + i;
-            var colTimeEl = $("<div>").addClass("col-2").text(x + "AM");
+            var colTimeEl = $("<div>").addClass("col-2 text-center border-right border-dark timebrdr").text(x + "AM");
         } else if (i === 3) {
             var x = 12
-            var colTimeEl = $("<div>").addClass("col-2").text(x + "PM");
+            var colTimeEl = $("<div>").addClass("col-2 text-center border-right border-dark timebrdr").text(x + "PM");
         } else {
             var x = i - 3;
-            var colTimeEl = $("<div>").addClass("col-2").text(x + "PM");
+            var colTimeEl = $("<div>").addClass("col-2 text-center border-right border-dark timebrdr").text(x + "PM");
         }
         // creates the task div
-        var colTaskEl = $("<div>").addClass("col-8").text("Tasks Go Here").attr("id", x + "task");
+        var colTaskEl = $("<div>").addClass("col-8 task").text("Tasks Go Here").attr("id", x + "task");
         // creates the button div
-        var colSaveEl = $("<div>").addClass("col-2").text("Button Goes Here");
+        var colSaveEl = $("<div>").addClass("col-2 border border-dark rounded-right").text("Button Goes Here");
         // appends the created columns to the row
         rowEl.append(colTimeEl, colTaskEl, colSaveEl);
         // appends the created row to the calendarEl in the html
         calendarEl.append(rowEl);
     }
 }
+
+$(".container").on("click",".task", function() {
+    var thisTask = $(this);
+    console.log(thisTask);
+});
 
 init();
