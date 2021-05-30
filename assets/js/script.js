9fc4eb150547904ba94f969234567790babbf1d5
Ryely <ryely.moore@outlook.com>
2021-05-29 21:23:56 -0600

    allows for tasks to be saved to local memory when the save button is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var calendarEl = $("#calendar")
+var calendarTasks = {};
 
 var init = function() {
     // Create a variable for the current date
@@ -6,6 +7,9 @@ var init = function() {
     console.log(todayDate);
     // Set the text for the currentDay div to todays date
     $("#currentDay").text(todayDate);
+    // loads locally stored tasks
+    var savedTasks = JSON.parse(localStorage.getItem("calendarTasks"));
+    console.log("savedTasks",savedTasks);
     // Creates Calendar Time Blocks
     for(var i = 0; i < 9; i++){
         // creates a row element
@@ -13,21 +17,31 @@ var init = function() {
         // creates the time column
         if(i <= 2) {
             var x = 9 + i;
-            var colTimeEl = $("<div>").addClass("col-2 text-center border-right border-dark timebrdr").text(x + "AM");
+            var colTimeEl = $("<div>").addClass("col-2 text-right border-right border-dark timebrdr").text(x + "AM");
         } else if (i === 3) {
             var x = 12
-            var colTimeEl = $("<div>").addClass("col-2 text-center border-right border-dark timebrdr").text(x + "PM");
+            var colTimeEl = $("<div>").addClass("col-2 text-right border-right border-dark timebrdr").text(x + "PM");
         } else {
             var x = i - 3;
-            var colTimeEl = $("<div>").addClass("col-2 text-center border-right border-dark timebrdr").text(x + "PM");
+            var colTimeEl = $("<div>").addClass("col-2 text-right border-right border-dark timebrdr").text(x + "PM");
         }
-        // creates the task div
-        var colTaskEl = $("<div>").addClass("col-8 task").attr("id", x + "task");
-        var colTaskSpan = $("<span>").text("Tasks Go Here");
+        // creates the task div and load in saved tasks
+        if (savedTasks) {
+            if (savedTasks[i]){
+            var colTaskEl = $("<div>").addClass("col-8 text-center task").attr("id", "task" + i);
+            var colTaskSpan = $("<span>").text(savedTasks[i]);
+            } else {
+                var colTaskEl = $("<div>").addClass("col-8 text-center task").attr("id", "task" + i);
+                var colTaskSpan = $("<span>").text("Enter Task Here");
+            }
+        } else {
+        var colTaskEl = $("<div>").addClass("col-8 text-center task").attr("id", "task" + i);
+        var colTaskSpan = $("<span>").text("Enter Task Here");
+        }
         colTaskEl.append(colTaskSpan);
         // creates the button div
         // var colSaveEl = $("<div>").addClass("col-2 border border-dark rounded-right");
-        var colSaveButton = $("<button>").addClass("col-2 border border-dark btn btn-primary btn-block").append("<span class='oi oi-file'></span><p>Save<p>")
+        var colSaveButton = $("<button>").addClass("col-2 border border-dark btn btn-primary btn-block").attr("id", "button" + i).append("<span class='oi oi-file'></span><p>Save<p>")
        
         // appends the created columns to the row
         rowEl.append(colTimeEl, colTaskEl, colSaveButton);
@@ -57,5 +71,11 @@ $(".container").on("blur",".task textarea", function () {
 })
 
 $(".container").on("click","button", function() {
-    console.log("Saved");
+    var buttonId = $(this).attr("id");
+    var hour = buttonId[buttonId.length - 1];
+    var task = $("#task" + hour).text().trim();
+    
+    calendarTasks[hour] = task;
+    console.log(calendarTasks);
+    localStorage.setItem("calendarTasks",JSON.stringify(calendarTasks));
 })
